78a028b4757a660ef04263ced55512a5cf7085eb
Bruce Shaw <battery4cid@gmail.com>
2019-03-17 14:05:19 -0400

    Added update function that demonstrates update functionality for contacts in address book
    - added debug functionality for update

---
+++ src/app/contacts/contacts.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { Contact, Contacts } from '@ionic-native/contacts/ngx';
+import { DebugService } from '../debug/debug.service';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class ContactsService {
+
+   constructor(private contacts: Contacts, private debugService: DebugService) { }
+
+   //TODO: maybe this isn't a contact object passed in, but instead our internal data structure to house contact info
+   updateContact(contactWithUpdates: Contact) {
+
+      //TODO: check that contact exists
+      var contactToUpdate;
+      this.findContactByName("David Second").then(contactFound => {
+         this.debugService.add("ContactsService.updateContact: Contact found");
+         if (contactFound != undefined && contactFound != null && contactFound.length > 0) {
+            contactToUpdate = contactFound[0];
+            //contactToUpdate.phoneNumbers[0] = new ContactField('mobile', '6471234567');
+            //contactToUpdate.phoneNumbers.push(new ContactField("mobile", "6471234567"))
+            contactToUpdate.phoneNumbers[0].value = "4448915645";
+            //new ContactField('mobile', '321-475-9999')
+            // contactToUpdate.phoneNumbers = [{
+            //    //id: "1", TODO: May have to manually remove id so it doesn't somehow get changed
+            //    pref: false,
+            //    type: "mobile",
+            //    value: "999-564-7897"
+            // }];
+            //contactToUpdate.save();
+            // contactToUpdate.save()
+            // .then(
+            //    (contactsFound) => {
+            //       this.debugService.add("ContactsService.updateContact: Contact update complete");
+            //    }, (error: any) => {
+            //       this.debugService.add("ContactsService.updateContact: Contact update failed");
+            //       this.debugService.add(error);
+            //    });
+            // console.log(result)
+            contactToUpdate.save(function (success) {
+               this.debugService.add("ContactsService.updateContact: Contact update complete");
+            }, function (error) {
+               this.debugService.add("ContactsService.updateContact: Contact update failed");
+               this.debugService.add(error);
+            });
+         }
+      }, (error: any) => {
+         //TODO: handle known error cases like denied permissions.
+         this.debugService.add("ContactsService.updateContact: Error finding contacts.");
+         this.debugService.add(error);
+      });
+   }
+
+   findContactByName(nameToSearch: string): Promise<Contact> {
+      var opts = {
+         filter: nameToSearch,
+         multiple: false,
+         //hasPhoneNumber: true,
+         fields: ['displayName', 'name']
+      };
+      return this.contacts.find(['*'], opts).then(
+         (contactsFound) => {
+            this.debugService.add("ContactsService.findContactByName: Contact found");
+            this.debugService.add(contactsFound);
+            return contactsFound;
+         },
+         (error: any) => {
+            //TODO: handle known error cases like denied permissions.
+            this.debugService.add("ContactsService.findContactByName: Error finding contacts.");
+            this.debugService.add(error);
+            return Promise.resolve(undefined);
+         }
+      );
+   }
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { DebugService } from '../debug/debug.service';
+import { Contacts, Contact, ContactField, ContactName, ContactFieldType } from '@ionic-native/contacts/ngx';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
    selector: 'app-tab3',
@@ -11,9 +13,15 @@ export class Tab3Page {
 
    private readonly UB_ADDRESS_BOOK_CONTACTS_KEY = 'UB_ADDRESS_BOOK_CONTACTS';
    debugStatements: String;
-   constructor(private storage: Storage, private debugService: DebugService) {
+   constructor(private storage: Storage, private debugService: DebugService, private contactService: ContactsService) {
 
    }
+
+   updateContact() {
+      var testContact = new Contact();
+      this.contactService.updateContact(testContact);
+   }
+
    deleteUBNetwork() {
       this.storage.set(this.UB_ADDRESS_BOOK_CONTACTS_KEY, undefined);
       console.log("UB network deleted");
